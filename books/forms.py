c2bafade96732bb24580459a6265332fbcd88052
Vítor Sousa <vitor.sousa@tridonic.com>
2024-01-05 12:15:30 +0000

    first commit

---
+++ books/forms.py
@@ -0,0 +1,9 @@
+# books/forms.py
+from django import forms
+from .models import Book
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'publish_date', 'in_stock', 'author']
